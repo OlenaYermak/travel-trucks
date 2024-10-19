ca01e73120509b501f6840af2e732f443d460044
OlenaYermak <145419464+OlenaYermak@users.noreply.github.com>
2024-10-19 14:35:56 +0300

    update
    deleting unused files

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,14 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider, Helmet } from "react-helmet-async";
-// import store from "./redux/store.js";
+
 import App from "../src/components/App/App.jsx";
 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <Provider store={store}> */}
     <HelmetProvider>
       <BrowserRouter>
         <Helmet>
@@ -23,6 +21,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <App />
       </BrowserRouter>
     </HelmetProvider>
-    {/* </Provider> */}
   </StrictMode>
 );
